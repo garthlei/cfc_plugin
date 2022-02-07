eb98b9384e3263b1a29dc67d46ea8c327df460f9
Bohan Lei <garthlei@pku.edu.cn>
2022-02-08 00:26:42 +0800

    feat: add crc and use new mnemonics

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-g++ -I`riscv64-unknown-elf-gcc -print-file-name=plugin`/include -I/usr/local/include -dynamiclib -undefined dynamic_lookup -fPIC -fno-rtti -o plugin.dylib plugin.cc func.cpp
+g++ -I`riscv64-unknown-elf-gcc -print-file-name=plugin`/include -I/usr/local/include -dynamiclib -undefined dynamic_lookup -fPIC -fno-rtti -o plugin.dylib plugin.cc
--- plugin.cc
+++ plugin.cc
@@ -12,12 +12,13 @@
 #include "gimple-iterator.h"
 #include "plugin-version.h"
 #include "tree-pass.h"
-#include "util.h"
 #include <iostream>
 #include <map>
 #include <vector>
 
 typedef uint8_t cfcss_sig_t;
+
+#define BUF_SIZE 64
 
 // This plugin is licensed under GPL.
 #ifdef _WIN32
@@ -72,6 +73,9 @@ unsigned int pass_cfcss::execute(function *fun) {
 
   // Basic block.
   basic_block bb;
+
+  // Instruction string buffer.
+  char inst[BUF_SIZE];
 
   push_cfun(fun);
 
@@ -144,18 +148,23 @@ unsigned int pass_cfcss::execute(function *fun) {
     cfcss_sig_t cur_diff = diff[bb];
     cfcss_sig_t cur_adj = dmap.find(bb) != dmap.end() ? dmap[bb] : 0;
 
-    if (bb->preds->length() >= 2)
-      stmt = gimple_build_asm_vec(inst_ctrlsig_m(cur_diff, cur_sig, cur_adj),
+    
+
+    if (bb->preds->length() >= 2) {
+      sprintf(inst, "ctrlsig_m %d,%d,%d", cur_diff, cur_sig, cur_adj);
+      stmt = gimple_build_asm_vec(inst,
                                   nullptr, nullptr, nullptr, nullptr);
-    else
-      stmt = gimple_build_asm_vec(inst_ctrlsig_s(cur_diff, cur_sig, cur_adj),
+    } else {
+      sprintf(inst, "ctrlsig_s %d,%d,%d", cur_diff, cur_sig, cur_adj);
+      stmt = gimple_build_asm_vec(inst,
                                   nullptr, nullptr, nullptr, nullptr);
+    }
     gimple_asm_set_volatile(stmt, true);
     gsi_insert_before(&gsi, stmt, GSI_NEW_STMT);
 
     if ((*bb->preds)[0]->src == fun->cfg->x_entry_block_ptr) {
       stmt = gimple_build_asm_vec(
-        ".insn r CUSTOM_1, 0, 0, x2, x0, x0",
+        "pushsig",
         nullptr, nullptr, nullptr, nullptr
       );
       gsi_insert_before(&gsi, stmt, GSI_SAME_STMT);
@@ -163,13 +172,25 @@ unsigned int pass_cfcss::execute(function *fun) {
       gimple_set_modified(stmt, false);
     }
 
+    gsi = gsi_last_bb(bb);
+    if (gimple_code(gsi_stmt(gsi)) == GIMPLE_COND
+        || gimple_code(gsi_stmt(gsi)) == GIMPLE_CALL
+        || gimple_code(gsi_stmt(gsi)) == GIMPLE_RETURN)
+      gsi_prev(&gsi);
+    stmt = gimple_build_asm_vec(
+      "crcsig 0",
+      nullptr, nullptr, nullptr, nullptr
+    );
+    gsi_insert_after(&gsi, stmt, GSI_NEW_STMT);
+    gimple_asm_set_volatile(stmt, true);
+    gimple_set_modified(stmt, false);
+
     if ((*bb->succs)[0]->dest == fun->cfg->x_exit_block_ptr) {
-      gsi = gsi_last_bb(bb);
       stmt = gimple_build_asm_vec(
-        ".insn r CUSTOM_1, 0, 0, x3, x0, x0",
+        "popsig",
         nullptr, nullptr, nullptr, nullptr
       );
-      gsi_insert_before(&gsi, stmt, GSI_SAME_STMT);
+      gsi_insert_after(&gsi, stmt, GSI_SAME_STMT);
       gimple_asm_set_volatile(stmt, true);
       gimple_set_modified(stmt, false);
     }
